cd8ea18b27720956c6d7f08ba05e7f19998e534a
Anjali8368 <99957334+Anjali8368@users.noreply.github.com>
2022-02-19 15:24:35 +0530

    Add files via upload

---
+++ Assignment Day 3.py
@@ -0,0 +1,58 @@
+# Question1: Find the data type of a if a=9.
+# Ans1.:- 9 is the type of  int class.
+
+
+# Question2: Find the data type of a if a=9
+# Ans2.:- 9 is the type of  int class.
+
+
+# Question3: Find the data type of a if a='9.'
+# Ans3.:- 9 is the type of  string class.
+
+
+# Question4: Find the data type of a if a=(9)
+# Ans4.:- 9 is the type of  int class.
+
+
+# Question5: Find the data type of a if a=False.
+# Ans5.:- 9 is the type of  boolean class.
+
+
+# Question6:  Find the data type of a if a=[1,2,3]
+# Ans6.:- 9 is the type of  list class.
+
+
+# Question7:  Find the data type of a if a=(1,2,3)
+# Ans7.:- 9 is the type of  tuple class.
+
+
+# Question8: Find the data type of a if a={'key': 9}
+# Ans8.:- 9 is the type of dictionary class.
+
+
+
+# Question9: Find the data type of a if a=1 + 9j
+# Ans9.:- 9 is the type of  complex class.
+
+
+# Question10: Set a=1 and b=2. What data type is a/b?
+# Ans10.:- 9 is the type of float class.
+
+
+# Question11:  Create a dictionary numbers = {'one':1, 'two':2, 'three':3}. Pull out the number '2' by calling the key 'two'
+# Ans11.:- 2
+# A={'one':1, 'two':2, 'three':3} 
+# print(A['two'])
+
+
+# Question12: Create a tuple with the numbers 8, 9, and 10?
+# Ans12.:-
+# tuple=(8,9,10)
+# print("Tuple: ", tuple)
+
+
+# Question13: Run the following lines of code and explain the error in your own words. Then rewrite the lines of code to run error free:
+# d = {one:1, two:2, three:3} d[one]
+# Ans13.:- In this dictionary one, two and three, these keys are not in single quetes and d has wrong syntex. error free code is written  below.
+# d={'one':1, 'two':2, 'three':3} 
+# print(d['one'])
